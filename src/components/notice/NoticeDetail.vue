1a554d298101f92ef88ea962e6700085b4915270
안태현 <dksxogus1012@gmail.com>
2022-11-19 18:16:17 +0900

    공지사항 디테일

--- src/components/notice/NoticeDetail.vue
+++ src/components/notice/NoticeDetail.vue
@@ -1,16 +1,41 @@
 <template>
   <div class="app">
     <div v-if="getNoticeObjectObserver != null">
-      {{ getNoticeObjectObserver.uid }}
-      <br />
-      {{ getNoticeObjectObserver.title }}
-      <br />
-      {{ getNoticeObjectObserver.content }}
-      <br />
-      {{ getNoticeObjectObserver.sdate }}
-      <br />
-      {{ getNoticeObjectObserver.udate }}
-      <br />
+      <v-row justify="center">
+        <v-col xl="8" align="left">
+          <v-row>
+            <v-col>
+              <div class="text-h4 font-weight-bold primary--text pt-4">
+                {{ getNoticeObjectObserver.title }}
+              </div>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>
+              <div style="color: grey">{{ getNoticeObjectObserver.udate }}</div>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>
+              <v-divider class="my-4"></v-divider>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col>
+              <div style="font-size: 20px">{{ getNoticeObjectObserver.content }}</div>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols="2">
+              <v-btn color="secondary" @click="_goBack">목록으로 돌아가기</v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
@@ -29,6 +54,9 @@ export default {
   },
   methods: {
     ...mapActions("noticeStore", ["getNoticeOne"]),
+    _goBack() {
+      this.$router.go(-1);
+    },
   },
   components: {},
 
