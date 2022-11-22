90661dc4214cfd9278c89e6227e7538e0c227b51
최진우 <cjw_git@naver.com>
2022-11-22 17:29:38 +0900

    카카오 로그인 적용

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // local vue api axios instance
 function apiInstance() {
   return axios.create({
-    baseURL: "http://localhost",
+    baseURL: "http://183.97.128.216",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
     },
